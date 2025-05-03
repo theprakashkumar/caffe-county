54a27f8b65a148f9eecc7edc6418554a4c27838f
theprakashkumar <maiprakashkumar@gmail.com>
2025-05-03 22:44:21 +0530

    feat: Added swagger for API doc.

--- apps/auth/src/main.ts
+++ apps/auth/src/main.ts
@@ -3,6 +3,9 @@ import cors from "cors";
 import cookieParser from "cookie-parser";
 import { errorMiddleware } from "../../../packages/error-handler/errorHandlerMiddleware";
 import authRouter from "./routes/auth.router";
+import swaggerUi from "swagger-ui-express";
+const swaggerDocument = require("./swagger.json");
+
 const app = express();
 
 app.use(express.json());
@@ -14,6 +17,11 @@ app.use(
     credentials: true,
   })
 );
+
+app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerDocument));
+app.get("/api-docs", (req, res) => {
+  res.json(swaggerDocument);
+});
 
 //Routes
 app.use("/api", authRouter);
@@ -27,6 +35,7 @@ app.use(errorMiddleware);
 const port = process.env.PORT || 6001;
 const server = app.listen(port, () => {
   console.log(`Auth Service is running on http://localhost:${port}`);
+  console.log(`API Docs: http://localhost:${port}/api-docs`);
 });
 
 server.on("error", (error) => {
---
+++ apps/auth/src/swagger.js
@@ -0,0 +1,16 @@
+import swaggerAutogen from "swagger-autogen";
+
+const doc = {
+  info: {
+    version: "1.0.0",
+    title: "Auth API",
+    description: "API for authentication.",
+  },
+  host: "localhost:6001",
+  schemes: ["http"],
+};
+
+const outputFile = "./swagger.json";
+const endpointsFiles = ["./routes/auth.router.ts"];
+
+swaggerAutogen(outputFile, endpointsFiles, doc);
